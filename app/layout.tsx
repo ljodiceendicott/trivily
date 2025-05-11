71ac4e539be965326a4977688e89b3f890180087
Luke Jodice <60670203+ljodiceendicott@users.noreply.github.com>
2025-05-10 23:38:04 -0400

    page title updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import './globals.css';
 
 import { GeistSans } from 'geist/font/sans';
 
-let title = 'Next.js + Postgres Auth Starter';
+let title = 'Trivily';
 let description =
-  'This is a Next.js starter kit that uses NextAuth.js for simple email + password login and a Postgres database to persist the data.';
+  'A Chronological History sorting Puzzle game';
 
 export const metadata = {
   title,
